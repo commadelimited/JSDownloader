8465978d381ce885189701db5a6b5155723973e6
andy matthews <andy@commadelimited.com>
2013-08-18 21:05:09 -0500

    Change target of click function.

--- public/js/app.js
+++ public/js/app.js
@@ -20,7 +20,7 @@ $(function(){
         $('#messages').append($message);
     });
 
-    $(document).on('click', '#links a', function(e){
+    $(document).on('click', '#recent a', function(e){
         var $a = $(this),
             $input = $('#source_url');
         $input.val($a.attr('href'));
